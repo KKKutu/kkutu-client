546346842648e9c58fe67e7d32b0ddc58f3ee248
MinseoKangQ <kms02171@naver.com>
2023-12-14 14:11:04 +0900

    Feat: 방 들어가는 화면 [메뉴] 이미지 to 자체, 액션리스너 추가

--- src/screen/ReadyToGame.java
+++ src/screen/ReadyToGame.java
@@ -1,16 +1,24 @@
 package screen;
 
+import static component.RoundedMenu.createRoundedPanel;
+
+import component.RoundedButton;
+import component.RoundedIdField;
+import component.RoundedPwField;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Objects;
 import javax.swing.ImageIcon;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ReadyToGame extends JFrame {
 
-    private static final Color BACKGROUND_COLOR = Color.decode("#F7F7F7");
     private static final Color LINE_COLOR = Color.decode("#DCDCDC");
 
     private JPanel menuPanel;
@@ -36,19 +44,65 @@ public class ReadyToGame extends JFrame {
         profilePanel = addPanel(0, 363, 276, 237, Color.RED);
         roomsPanel = addPanel(278, 52, 724, 546, Color.BLUE);
 
-        // 메뉴 부착
-        // 이미지 라벨 추가
-        JLabel informationLabel = createImageLabel("../image/readytogame/information.png", 0, 0, 50, 50);
-        menuPanel.add(informationLabel);
+        // 패널 추가 및 식별자 설정
+        JPanel informationPanel = createRoundedPanel(0, 0, 50, 50, Color.decode("#BBBBBB"), 15);
+        JPanel settingPanel = createRoundedPanel(50, 0, 50, 50, Color.decode("#CCCCCC"), 15);
+        JPanel profilePanel = createRoundedPanel(100, 0, 163, 50, Color.decode("#E3E3E3"), 15);
+        JPanel makeRoomPanel = createRoundedPanel(263, 0, 237, 50, Color.decode("#8DC0F2"), 15);
 
-        JLabel settingLabel = createImageLabel("../image/readytogame/setting.png", 50, 0, 50, 50);
-        menuPanel.add(settingLabel);
+        // 레이아웃 매니저 설정
+        informationPanel.setLayout(null);
+        settingPanel.setLayout(null);
+        profilePanel.setLayout(null);
+        makeRoomPanel.setLayout(null);
 
-        JLabel changeProfileLabel = createImageLabel("../image/readytogame/change_profile.png", 100, 0, 163, 50);
-        menuPanel.add(changeProfileLabel);
+        // 각 패널에 컨텐츠 추가
+        addContentToPanel(informationPanel, "../image/readytogame/information.png", 50, 50);
+        addContentToPanel(settingPanel, "../image/readytogame/setting.png", 50, 50);
+        addCenteredTextToPanel(profilePanel, "프로필 변경", 20);
+        addCenteredTextToPanel(makeRoomPanel, "방 만들기", 20);
 
-        JLabel makeRoomLabel = createImageLabel("../image/readytogame/make_room.png", 263, 0, 163, 50);
-        menuPanel.add(makeRoomLabel);
+        // 메뉴 패널에 추가
+        menuPanel.add(informationPanel);
+        menuPanel.add(settingPanel);
+        menuPanel.add(profilePanel);
+        menuPanel.add(makeRoomPanel);
+
+        // informationPanel 에 마우스 리스너 추가
+        informationPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // 게임 소개, 개발자 정보 다이얼로그 보여주기
+                System.out.println("게임 소개, 개발자 정보 다이얼로그 보여주기");
+//                showInformationDialog();
+            }
+        });
+
+        // settingPanel 에 마우스 리스너 추가
+        settingPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("게임 환경 설정 다이얼로그 보여주기");
+//                showSettingDialog();
+            }
+        });
+
+        // profilePanel 에 마우스 리스너 추가
+        profilePanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                showProfileDialog();
+            }
+        });
+
+        // informationPanel 에 마우스 리스너 추가
+        makeRoomPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // 다음 화면으로 넘어가기
+                System.out.println("다음 화면으로 넘어갑니다");
+            }
+        });
 
         // 선 그리기
         addLinePanel();
@@ -94,14 +148,47 @@ public class ReadyToGame extends JFrame {
         );
     }
 
-    // 이미지를 라벨로
+    // 이미지를 라벨로 (위치 조정)
     private JLabel createImageLabel(String imagePath, int x, int y, int width, int height) {
         JLabel imgLabel = new JLabel();
         ImageIcon icon = loadIcon(imagePath);
         imgLabel.setIcon(icon);
         imgLabel.setBounds(x, y, width, height);
-        imgLabel.setHorizontalAlignment(JLabel.CENTER);
+        imgLabel.setLayout(null);  // 레이아웃 매니저 설정
         return imgLabel;
     }
 
+    // 패널에 이미지 추가 (가운데 정렬)
+    private void addContentToPanel(JPanel panel, String imagePath, int imgWidth, int imgHeight) {
+        JLabel imgLabel = createImageLabel(imagePath, (panel.getWidth() - imgWidth) / 2, (panel.getHeight() - imgHeight) / 2, imgWidth, imgHeight);
+        panel.add(imgLabel);
+    }
+
+    // 패널에 텍스트 추가 (가운데 정렬)
+    private void addCenteredTextToPanel(JPanel panel, String text, int fontSize) {
+        JLabel textLabel = createTextLabel(text, fontSize);
+        textLabel.setSize(panel.getWidth(), panel.getHeight());
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setVerticalAlignment(JLabel.CENTER);
+        panel.add(textLabel);
+    }
+
+    // 텍스트 라벨 생성 (크기 조정 없음)
+    private JLabel createTextLabel(String text, int fontSize) {
+        JLabel label = new JLabel(text);
+        label.setFont(new Font("Dialog", Font.PLAIN, fontSize));
+        return label;
+    }
+
+    // 프로필 변경 다이얼로그 표시 메서드
+    private void showProfileDialog() {
+        JDialog dialog = new JDialog(this, "프로필 변경", true); // true로 설정하여 모달로 만듦
+        dialog.setLayout(null);
+        dialog.setTitle("프로필 변경");
+        dialog.setSize(600, 350); // 다이얼로그 크기 설정
+        dialog.setResizable(false);
+        dialog.setLocationRelativeTo(null); // 화면 중앙에 위치
+
+        dialog.setVisible(true); // 다이얼로그 보이기
+    }
 }
