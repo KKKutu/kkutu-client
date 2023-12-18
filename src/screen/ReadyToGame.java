178ce06b3b41fecbc00f865a819c576f822d1130
xogk1128 <xogk1128@naver.com>
2023-12-18 23:33:24 +0900

    feat: 배경음악 추가

--- src/screen/ReadyToGame.java
+++ src/screen/ReadyToGame.java
@@ -1,23 +1,23 @@
 package screen;
 
-import static component.RoundedMenu.createRoundedPanel;
-
+import bgm.MP3Player;
 import component.RoundedButton;
 import component.RoundedIdField;
 import component.RoundedTitle;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.*;
 import java.net.Socket;
-import java.util.List;
 import java.util.Objects;
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
+
+import static component.RoundedMenu.createRoundedPanel;
+
 
 public class ReadyToGame extends JFrame {
 
@@ -49,6 +49,14 @@ public class ReadyToGame extends JFrame {
         try {
             output = new DataOutputStream(socket.getOutputStream());
             input = new DataInputStream(socket.getInputStream());
+
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    new Thread(new MP3Player()).start();
+                }
+            });
+
 //            enterGame();
             // UpdateThread 시작
             setScreen();
